7bde58eceaaac63249490ca4714197184c96a141
Kutarumo <enzo.mrt66@gmail.com>
2023-10-06 16:36:23 +0200

    fix: add readme, create assets folder

--- game.js
+++ game.js
@@ -1,10 +1,3 @@
 const app = new PIXI.Application({ width: 800, height: 600 });
 
 document.body.appendChild(app.view);
-
-const square = new PIXI.Graphics();
-square.beginFill(0xFF0000);
-square.drawRect(100, 100, 200, 200);
-square.endFill();
-
-app.stage.addChild(square);
