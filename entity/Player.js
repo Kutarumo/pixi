59bd84dadeef3e5b880815acf12636cf2f7b0731
fr33zingH3ll <samuelnicod66@gmail.com>
2023-10-07 08:56:00 +0200

    feat: class player and sizing game

--- entity/Player.js
+++ entity/Player.js
@@ -1,3 +1,61 @@
 class Player {
+    constructor(app) {
+        this.app = app;
+        this.sprite = null;
+        this.isLeftKeyDown = false;
+        this.isRightKeyDown = false;
     
+        this.loader = new PIXI.Loader();
+    
+        this.loader.add('python', './python.png').load((loader, resources) => {
+            console.log(resources);
+  
+            if (resources.python) {
+                this.createSprite(resources.python.texture);
+                this.setupEventListeners();
+                this.app.ticker.add(this.update.bind(this));
+            } else {
+                console.error("L'image 'python' n'a pas été chargée correctement.");
+            }
+        });
+    }
+  
+    createSprite(texture) {
+        this.sprite = new PIXI.Sprite(texture);
+        this.sprite.scale.set(0.05, 0.05);
+        this.sprite.x = 250;
+        this.sprite.y = 350;
+        this.app.stage.addChild(this.sprite);
+    }
+  
+    setupEventListeners() {
+        window.addEventListener('keydown', this.handleKeyDown.bind(this));
+        window.addEventListener('keyup', this.handleKeyUp.bind(this));
+    }
+  
+    handleKeyDown(event) {
+        if (event.key === 'ArrowLeft') { this.isLeftKeyDown = true; } 
+        else if (event.key === 'ArrowRight') { this.isRightKeyDown = true; }
+    }
+  
+    handleKeyUp(event) {
+        if (event.key === 'ArrowLeft') { this.isLeftKeyDown = false; } 
+        else if (event.key === 'ArrowRight') { this.isRightKeyDown = false; }
+    }
+  
+    update() {
+        const playerSpeed = 5;
+        if (this.isLeftKeyDown) {
+            if (this.sprite.x - playerSpeed >= 0) {
+                this.sprite.x -= playerSpeed;
+            }
+        }
+        if (this.isRightKeyDown) {
+            if (this.sprite.x + this.sprite.width + playerSpeed <= this.app.renderer.width) {
+                this.sprite.x += playerSpeed;
+            }
+        }
+    }
 }
+  
+export { Player };
--- game.js
+++ game.js
@@ -1,64 +1,13 @@
-const app = new PIXI.Application({ width: 800, height: 600 });
+import { Player } from './entity/Player.js';
+
+const size = 500;
+
+const app = new PIXI.Application({ width: size, height: size });
 document.body.appendChild(app.view);
 
-const loader = new PIXI.Loader();
-
-let sprite; // Déclarez une variable pour stocker le sprite
-
-loader.add('python', './python.png').load((loader, resources) => {
-  console.log(resources); // Affiche les ressources chargées dans la console
-
-  // Vérifiez que l'image est correctement chargée avant de créer le sprite
-  if (resources.python) {
-    // Créez le sprite
-    sprite = new PIXI.Sprite(resources.python.texture);
-
-    // Ajustez la taille du sprite (réduction à 50%)
-    sprite.scale.set(0.05, 0.05);
-
-    sprite.x = 100;
-    sprite.y = 100;
-    app.stage.addChild(sprite);
-
-    // Ajoutez un gestionnaire d'événement pour les touches de clavier
-    window.addEventListener('keydown', handleKeyDown);
-    window.addEventListener('keyup', handleKeyUp);
-  } else {
-    console.error("L'image 'python' n'a pas été chargée correctement.");
-  }
-});
-
-let isLeftKeyDown = false;
-let isRightKeyDown = false;
-
-function handleKeyDown(event) {
-  if (event.key === 'ArrowLeft') {
-    isLeftKeyDown = true;
-  } else if (event.key === 'ArrowRight') {
-    isRightKeyDown = true;
-  }
-}
-
-function handleKeyUp(event) {
-  if (event.key === 'ArrowLeft') {
-    isLeftKeyDown = false;
-  } else if (event.key === 'ArrowRight') {
-    isRightKeyDown = false;
-  }
-}
-
-// Fonction pour mettre à jour la position du sprite en fonction des touches enfoncées
-function updateSpritePosition() {
-  if (isLeftKeyDown) {
-    sprite.x -= 5; // Déplace le sprite vers la gauche
-  }
-
-  if (isRightKeyDown) {
-    sprite.x += 5; // Déplace le sprite vers la droite
-  }
-}
+const player = new Player(app);
 
 // Appel de la fonction updateSpritePosition à chaque rafraîchissement de l'écran
 app.ticker.add(() => {
-  updateSpritePosition();
+  player.update();
 });
